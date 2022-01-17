4509b488f9df119de3a8b7c7356637688024345b
Adam Thometz <adamthometz@gmail.com>
2022-01-17 18:41:27 -0500

    made intro component

---
+++ src/intro/Intro.js
@@ -0,0 +1,36 @@
+import React from "react";
+import NavContact from "../navbar-contact/NavContact";
+import "./Intro.css"
+import Lottie from "react-lottie";
+import spinningStack from './animation/spinning-stack.json'
+
+const Intro = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: spinningStack,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  }
+  return (
+    <div className="Intro">
+      <div className="Intro-header-animation-container">
+        <div className="Intro-header" tabIndex={0}>
+          <h1>Hello! My name is Adam Thometz</h1>
+          <h2>I am a software engineer specializing in full-stack development and accessibility based in NYC.</h2>
+        </div>
+        <div className="Intro-animation" tabIndex={-1}>
+          <Lottie options={defaultOptions} width={400} height={300} />
+        </div>
+      </div>
+
+      <div>
+        <p tabIndex={0}>Pick where you want to go</p>
+        <NavContact />
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
