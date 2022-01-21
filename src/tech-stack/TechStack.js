979b3d02477294d40f855321ee042a535b833cbd
Adam Thometz <adamthometz@gmail.com>
2022-01-21 12:10:03 -0500

    removed stray console.log

--- src/tech-stack/TechStack.js
+++ src/tech-stack/TechStack.js
@@ -5,12 +5,11 @@ import './TechStack.css'
 
 const TechStack = ({ techStack }) => {
   return (
-    <div className="TechStack" tabIndex={0}>
+    <div className="TechStack">
       <h3>Tech Stack Used</h3>
       <div className="TechStack-main">
         {techStack.map(tech => {
           const t = techStackInfo.find(currTech => currTech.name === tech)
-          console.log(t)
           return <TechStackCard name={t.name} icon={t.icon} />
         })}
       </div>
