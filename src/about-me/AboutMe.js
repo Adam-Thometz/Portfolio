119e8798b7eaa4977165a7d932786247df7e36db
Adam Thometz <adamthometz@gmail.com>
2022-08-26 17:34:52 -0400

    updated resume

--- src/about-me/AboutMe.js
+++ src/about-me/AboutMe.js
@@ -17,7 +17,7 @@ const AboutMe = () => {
           <p>Before becoming a software engineer, I worked in a variety of special education settings, most recently as a music teacher for autistic students in grades K-2.</p>
           <p>I've been fascinated by technology's potential since I started producing my own music as a teenager but I was truly inspired to start programming after discovering <Link to="#" onClick={() => window.open("https://musiclab.chromeexperiments.com")}>Google's Chrome Music Lab</Link> and trying it out in my music classroom. From that point on, I started teaching myself how to code in Python and Javascript on and off and then eventually, enrolled in Springboard's software engineering program, where I learned about frontend and backend technologies and frameworks.</p>
           <p>When I'm not building (or learning about building) software or composing music, I do martial arts, read anything related to philosophy, tend to my indoor garden, and spend time with my pet blue tongue skink (if you're a reptile lover too, check out Reptifeed in my Projects section!)</p>
-          <b>I'm currently seeking a role in frontend web development! <Link to="#" onClick={() => window.open("https://resume.creddle.io/resume/o9lpco7gae")}>Click here to check out my resume.</Link></b>
+          <b>I'm currently seeking a role in frontend web development! <Link to="#" onClick={() => window.open("https://docs.google.com/document/d/18oRb8BghIT_kmwTY2EXB03Lq9laVucn2t3BIKQmun7Y/edit?usp=sharing")}>Click here to check out my resume.</Link></b>
         </div>
       </div>
       <NavContact />
