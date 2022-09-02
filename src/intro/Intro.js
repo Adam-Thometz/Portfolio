afea8c733aba565bf6bf7eebff5ad3ec31bd4e77
Adam Thometz <adamthometz@gmail.com>
2022-09-02 13:41:46 -0400

    changed copy

--- src/intro/Intro.js
+++ src/intro/Intro.js
@@ -19,7 +19,7 @@ const Intro = () => {
       <div className="Intro-header-animation-container">
         <div className="Intro-header" tabIndex={0}> 
           <h1>Hello! My name is Adam Thometz</h1>
-          <h2>I am a full-stack developer specializing in front-end web development based in NYC.</h2>
+          <h2>I am a full-stack developer specializing in frontend development based in NYC.</h2>
         </div>
         <div className="Intro-animation" tabIndex={-1}>
           <Lottie options={defaultOptions} width={400} height={300} />
