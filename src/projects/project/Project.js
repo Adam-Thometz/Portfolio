5a48b16a798779fca08359ac3cec4635fdd27b11
Adam Thometz <adamthometz@gmail.com>
2022-09-01 21:25:20 -0400

    refactor

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -1,13 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import TechStack from "../tech-stack/TechStack";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import './Project.css';
 
-const Project = ({name, award, description, learnedHowTo, techStack, login, img, links}) => {
+const Project = ({ name, award, description, learnedHowTo, techStack, login, img, links }) => {
   const cssClass = name.toLowerCase().replaceAll(' ', '-').replace('8', 'eight');
+
+  const openDemo = () => window.open(links.demo);
+  const openGithub = () => window.open(links.github);
+
+  const loginDisplay = login ? (
+    <div className="Project-login-info">
+      <b>To fully explore the app, make your own account or log in with the following:</b>
+      <ul>
+        <li>Username: {login.username}</li>
+        <li>Password: {login.password}</li>
+      </ul>
+    </div>
+  ) : null;
+
+  const awardDisplay = award ? <img className="Project-award" src={award} alt={`An award for ${name}`} /> : null;
+
+  const demo = <FontAwesomeIcon title={`Demo for ${name}`} icon={faGlobe} />;
+  const github = <FontAwesomeIcon title={`Github repo for ${name}`} icon={faGithub} />;
+
+  const linkDisplay = <div className="Project-links">
+    {links.demo ? <Link className="demo" to="" onClick={openDemo}>{demo}</Link> : null}
+    <Link className="github" to="" onClick={openGithub}>{github}</Link>
+  </div>
 
   return (
     <div className={`Project ${cssClass}`} key={cssClass}>
@@ -20,27 +45,14 @@ const Project = ({name, award, description, learnedHowTo, techStack, login, img,
           <div className="Project-description">
             <h2>Description</h2>
             {description}
-            {learnedHowTo ? <>
-              <h2>Building this taught me how to...</h2>
-              <ul>
-                {learnedHowTo.map(fact => <li>{fact}</li>)}
-              </ul>
-            </> : null}
-            {award ? <img className="Project-award" src={award} alt={`An award for ${name}`} /> : null}
+            <h2>Building this taught me how to...</h2>
+            <ul>
+              {learnedHowTo.map(fact => <li>{fact}</li>)}
+            </ul>
+            {awardDisplay}
           </div>
-            {login ? (
-              <div className="Project-login-info">
-                <b>To fully explore the app, make your own account or log in with the following:</b>
-                <ul>
-                  <li>Username: {login.username}</li>
-                  <li>Password: {login.password}</li>
-                </ul>
-              </div>
-            ) : null}
-          <div className="Project-links">
-            {links.demo ? <Link className="demo" to="" onClick={() => window.open(links.demo)}><FontAwesomeIcon title={`Demo for ${name}`} icon={faGlobe} /></Link> : null}
-            <Link className="github" to="" onClick={() => window.open(links.github)}><FontAwesomeIcon title={`Github repo for ${name}`} icon={faGithub} /></Link>
-          </div>
+          {loginDisplay}
+          {linkDisplay}
         </div>
       </div>
       <TechStack techStack={techStack} />
