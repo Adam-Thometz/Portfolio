f0a925584dea1aafdb715ad2bb5b0b5507a2094f
Adam Thometz <adamthometz@gmail.com>
2022-09-02 13:29:16 -0400

    created nicer 404 page

---
+++ src/404/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import './NotFound.css';
+
+const NotFound = () => {
+  return <main className="NotFound">
+    <h1>You've got lost in space!</h1>
+    <h2>Enjoy the scenery then pick your next stop in the navbar below</h2>
+  </main>
+};
+
+export default NotFound;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
-import Navbar from './_components/navbar/Navbar';
 import Intro from './intro/Intro';
 import Projects from './projects/Projects';
-import NotFound from './NotFound';
 import AboutMe from './about-me/AboutMe';
 import Contact from './contact/Contact';
+import NotFound from './404/NotFound';
+import Navbar from './_components/navbar/Navbar';
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
         <Route path='/contact' element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Navbar />
+      <Navbar />s
     </div>
   );
 };
