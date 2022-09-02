13afca007d7f19d937bcc355be1fde30d2fe7754
Adam Thometz <adamthometz@gmail.com>
2022-09-02 13:17:06 -0400

    added data

--- src/_data/projectInfo.js
+++ src/_data/projectInfo.js
@@ -12,7 +12,16 @@ const projects = [
   {
     name: 'Art Buddy',
     description: <><p>First 'real-world' project since graduating from bootcamp. Art Buddy is a collection of arts-based games and tools designed specifically for the special education classroom and reaching students' academic goals. These games are based on real lessons I taught during my time as a music teacher at an elementary school for autism. Art Buddy is a collaboration between myself and UX designer/fellow educator <a href='https://uxfol.io/mattiebecker'>Mattie Becker</a>. This app is currently in progress - look at the README in Github to see a list of currently functioning games.</p> <p><b>The app was designed for a large screen so it will look terrible on mobile!</b></p></>,
-    learnedHowTo: ['make my code readable, obvious, and testable (I call it "making my code ROT")', 'make music play in the browser depending on app settings (i.e. guitar vs synth, C major vs F# major. I could play Baby Shark in C on guitar or A on synth)', 'create a component library of common UI elements like buttons, icons, dropdowns, popups, etc.', 'use package.json scripts to minimize typing in the terminal', 'incorporate user feedback into my features', 'articulate my vision for a product', 'mock files and stylesheets in testing environments', 'create relative imports'],
+    learnedHowTo: [
+      'make my code readable, obvious, and testable (I call it "making my code ROT")',
+      'make music play in the browser depending on app settings (i.e. guitar vs synth, C major vs F# major. I could play Baby Shark in C on guitar or A on synth)',
+      'create a component library of common UI elements like buttons, icons, dropdowns, popups, etc.',
+      'use package.json scripts to minimize typing in the terminal',
+      'incorporate user feedback into my features',
+      'articulate my vision for a product',
+      'mock files and stylesheets in testing environments',
+      'create relative imports'
+    ],
     techStack: ["Javascript", "React", "CSS", "Redux", "Jest"],
     img: artBuddy,
     links: {
@@ -23,8 +32,14 @@ const projects = [
   {
     name: 'Reptifeed',
     description: <p>My second and final capstone project for Springboard. Inspired by my blue-tongue skink, Unity, this app gives you ideas on what to feed your reptile and helps you keep track of the foods you have for them.</p>,
-    learnedHowTo: ['invent my own reptile diet API, since consistent data on reptile diets was hard to find', 'cycle through what they should eat based on a food\'s frequency and persist data (I used localStorage but looking back, I should have used a database)', 'build admin-only features', 'make a website/app accessible', 'make a responsive website'],
-    techStack: ["Javascript", "Node", "Express", "React", "CSS", "SQL", "Postgres", "Jest"],
+    learnedHowTo: [
+      'invent my own reptile diet API, since consistent data on reptile diets was hard to find',
+      'cycle through what they should eat based on a food\'s frequency and persist data (I used localStorage but looking back, I should have used a database)',
+      'build admin-only features',
+      'make a website/app accessible',
+      'make a responsive website'
+    ],
+    techStack: ["Javascript", "Node", "Express", "React", "CSS", "Postgres", "Jest"],
     login: {
       username: 'abc',
       password: '123'
@@ -39,8 +54,12 @@ const projects = [
     name: '8-Bit Clan',
     award: mintbeanStar,
     description: <p>A pixel art maker created for the Mintbean Hiring Hackathon, Nov 2021. Create art, save it to your profile, and see if it made it to the trending list! Built with UX designer <a href='https://melodyux.design/'>Melody Acosta</a> in a week. Winner of Mintbean Star, awarded to top 5% of competitors.</p>,
-    learnedHowTo: ['collaborate with a UX designer', 'work efficiently under tight time constraints', 'store art data in a database and call on that data to reproduce a smaller version of that art'],
-    techStack: ["Javascript", "Node", "Express", "React", "JSS", "SQL", "Postgres", "Heroku"],
+    learnedHowTo: [
+      'collaborate with a UX designer',
+      'work efficiently under tight time constraints',
+      'store art data in a database and call on that data to reproduce a smaller version of that art'
+    ],
+    techStack: ["Javascript", "Node", "Express", "React", "JSS", "Postgres"],
     login: {
       username: 'abc',
       password: '123'
@@ -53,9 +72,12 @@ const projects = [
   },
   {
     name: 'Catch Your Feelings',
-    description: <p>An app that I invented for helping students identify their emotions. You describe how happy and energetic you feel using icons and you get an emotion and a song back based on the input. <b>This project is now deprecated and the concept is currently being incorporated into Art Buddy as the Mood Meter!</b></p>,
-    learnedHowTo: ['apply my programming skills to solving a real problem', 'work with only vanilla JS - no frontend framework'],
-    techStack: ["Python", "Flask", "HTML", "CSS", "Javascript", "Heroku"],
+    description: <><p>An app that I invented for helping students identify their emotions. You describe how happy and energetic you feel using icons and you get an emotion and a song back based on the input.</p><p><b>This project is now deprecated and the concept is currently being incorporated into Art Buddy as the Mood Meter!</b></p></>,
+    learnedHowTo: [
+      'apply my programming skills to solving a real problem',
+      'work with only vanilla JS - no frontend framework'
+    ],
+    techStack: ["Python", "Flask", "HTML", "CSS", "Javascript"],
     img: catchYourFeelings,
     links: {
       github: 'https://github.com/Adam-Thometz/Catch-Your-Feelings'
@@ -64,8 +86,11 @@ const projects = [
   {
     name: 'Musophile',
     description: <p>My first capstone project for Springboard. A website where you can curate your own music library. Created using the Spotify and MusicBrainz APIs.</p>,
-    learnedHowTo: ['connect frontend and backend using server-side rendering', 'use the Spotify API and OAuth'],
-    techStack: ["Python", "Flask", "Postgres", "HTML", "CSS", "Javascript", "jQuery", "Heroku", "Bootstrap"],
+    learnedHowTo: [
+      'connect frontend and backend using server-side rendering',
+      'use the Spotify API and OAuth'
+    ],
+    techStack: ["Python", "Flask", "Postgres", "HTML", "CSS", "Javascript", "jQuery", "Bootstrap"],
     login: {
       username: 'abc',
       password: '12345678'
--- src/_data/techStackInfo.js
+++ src/_data/techStackInfo.js
@@ -8,13 +8,10 @@ import postgresql from '../_media/tech-stack-icons/postgresql.png';
 import nodeJS from '../_media/tech-stack-icons/nodeJS.png';
 import expressJS from '../_media/tech-stack-icons/expressJS.png';
 import reactJS from '../_media/tech-stack-icons/reactJS.png';
-import jasmine from '../_media/tech-stack-icons/jasmine.png';
 import jest from '../_media/tech-stack-icons/jest.png';
 import jQuery from '../_media/tech-stack-icons/jquery.png';
 import jss from '../_media/tech-stack-icons/jss.png'
-import reactTestingLibrary from '../_media/tech-stack-icons/react-testing-library.png';
 import bootstrap from '../_media/tech-stack-icons/bootstrap.png';
-import heroku from '../_media/tech-stack-icons/heroku.jpeg';
 import redux from '../_media/tech-stack-icons/redux.png';
 
 const techStackInfo = [
@@ -28,13 +25,10 @@ const techStackInfo = [
   {name: 'Node', icon: nodeJS},
   {name: 'Express', icon: expressJS},
   {name: 'React', icon: reactJS},
-  {name: 'Jasmine', icon: jasmine},
   {name: 'Jest', icon: jest},
-  {name: 'React Testing Library', icon: reactTestingLibrary},
   {name: 'jQuery', icon: jQuery},
   {name: 'JSS', icon: jss},
   {name: 'Bootstrap', icon: bootstrap},
-  {name: 'Heroku', icon: heroku},
   {name: 'Redux', icon: redux}
 ];
 
