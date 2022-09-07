8fe1a0d67e14513c3a2ac709649a468254ed9c91
Adam Thometz <adamthometz@gmail.com>
2022-09-06 22:39:40 -0400

    removed soundcloud link

--- src/navbar/links/Links.js
+++ src/navbar/links/Links.js
@@ -5,26 +5,26 @@ import './Links.css';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub, faSoundcloud } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { links } from '../../_data/navbarInfo';
 
 const Links = () => {
   const openResume = () => window.open(links.resume);
   const openLinkedIn = () => window.open(links.linkedIn);
   const openGithub = () => window.open(links.github);
-  const openSoundcloud = () => window.open(links.soundCloud);
+  // const openSoundcloud = () => window.open(links.soundCloud);
 
   const resume = <FontAwesomeIcon title="Resume" icon={faFile} />;
   const linkedIn = <FontAwesomeIcon title="LinkedIn" icon={faLinkedin} />;
   const github = <FontAwesomeIcon title="Github" icon={faGithub} />;
-  const soundcloud = <FontAwesomeIcon title="Soundcloud" icon={faSoundcloud} />;
+  // const soundcloud = <FontAwesomeIcon title="Soundcloud" icon={faSoundcloud} />;
 
   return (
     <section className='Links'>
       <Link className="Links-link resume" to="" onClick={openResume}>{resume}</Link>
       <Link className="Links-link linkedin" to="" onClick={openLinkedIn}>{linkedIn}</Link>
       <Link className="Links-link github" to="" onClick={openGithub}>{github}</Link>
-      <Link className="Links-link soundcloud" to="" onClick={openSoundcloud}>{soundcloud}</Link>
+      {/* <Link className="Links-link soundcloud" to="" onClick={openSoundcloud}>{soundcloud}</Link> */}
     </section>
   );
 };
