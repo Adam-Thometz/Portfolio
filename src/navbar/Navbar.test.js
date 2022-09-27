687c1c66fed4c4ceebe059c4196ddd614a66cdee
Adam Thometz <adamthometz@gmail.com>
2022-09-27 09:55:45 -0400

    moved nav to the right and added tests

---
+++ src/navbar/Navbar.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Navbar from './Navbar';
+
+describe('Navbar component', () => {
+  it('renders without crashing', () => {
+    render(<Navbar />);
+  });
+
+  it('matches the snapshot of the landing page displaying the games page', () => {
+    const { asFragment } = render(<Navbar />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
