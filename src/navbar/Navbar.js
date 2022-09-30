3c6ec904422dd4961eb10fab664c70103bf87379
Adam Thometz <adamthometz@gmail.com>
2022-09-29 22:04:44 -0400

    added twitter link

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -4,20 +4,22 @@ import './Navbar.css';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub, faSoundcloud } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub, faSoundcloud, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { links } from './navbarInfo';
 
 const Navbar = ({ style }) => {
   const github = <FontAwesomeIcon icon={faGithub} />;
   const linkedIn = <FontAwesomeIcon icon={faLinkedin} />;
+  const twitter = <FontAwesomeIcon icon={faTwitter} />;
   const resume = <FontAwesomeIcon icon={faFile} />;
   const soundcloud = <FontAwesomeIcon icon={faSoundcloud} />;
 
   return (
     <nav className='Navbar' style={style}>
+      <a className="Navbar-link resume" href={links.resume} title='Resume' target="_blank" rel='noreferrer'>{resume}</a>
       <a className="Navbar-link github" href={links.github} title='Github' target="_blank" rel='noreferrer'>{github}</a>
       <a className="Navbar-link linkedin" href={links.linkedIn} title='LinkedIn' target="_blank" rel='noreferrer'>{linkedIn}</a>
-      <a className="Navbar-link resume" href={links.resume} title='Resume' target="_blank" rel='noreferrer'>{resume}</a>
+      <a className="Navbar-link twitter" href={links.twitter} title='Twitter' target="_blank" rel='noreferrer'>{twitter}</a>
       <a className="Navbar-link soundcloud" href={links.soundCloud} title='Soundcloud' target="_blank" rel='noreferrer'>{soundcloud}</a>
     </nav>
   );
--- src/navbar/navbarInfo.js
+++ src/navbar/navbarInfo.js
@@ -1,6 +1,7 @@
 export const links = {
   linkedIn: "https://www.linkedin.com/in/adam-thometz/",
   github: "https://github.com/Adam-Thometz",
+  twitter: "https://twitter.com/code_to_joy",
   soundCloud: "https://soundcloud.com/this-is-psychic-hamster",
   resume: "http://shorturl.at/cFWYZ"
 };
