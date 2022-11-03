65fbdbdb40080179507855325fd48b177cf06ca7
Adam Thometz <adamthometz@gmail.com>
2022-11-03 15:13:51 -0400

    added tests for music component

---
+++ src/music/Music.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Music from './Music';
+
+describe('Music component', () => {
+  it('renders without crashing', () => {
+    render(<Music />);
+  });
+
+  it('matches the snapshot', () => {
+    const { asFragment } = render(<Music />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
