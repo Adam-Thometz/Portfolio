c603a72efa000828ed632068a26519598393905c
Adam Thometz <adamthometz@gmail.com>
2022-11-03 14:47:49 -0400

    replaced tabIndex with aria label

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -8,7 +8,14 @@ const Navbar = ({ style }) => {
   return (
     <nav className='Navbar' style={style}>
       {links.map(link => (
-        <a tabIndex={0} className={`Navbar-link ${link.id}`} href={links.url} title={link.id} target="_blank" rel='noreferrer'>{link.icon}</a>
+        <a
+          aria-label={`Link to ${link.id}`}
+          className={`Navbar-link ${link.id}`}
+          href={links.url}
+          title={link.id}
+          target="_blank"
+          rel='noreferrer'
+        >{link.icon}</a>
       ))}
     </nav>
   );
