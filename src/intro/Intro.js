7d5614b11a39e2439ff656ce2a5dca26d1e1e478
Adam Thometz <adamthometz@gmail.com>
2022-11-03 14:45:07 -0400

    removed unecessary tabIndexes

--- src/intro/Intro.js
+++ src/intro/Intro.js
@@ -5,10 +5,10 @@ import "./Intro.css"
 const Intro = ({ style }) => {
   return (
     <header className="Intro" style={style}>
-      <h1 tabIndex={0}>Hi, I'm Adam! 👋🏼</h1>
-      <h2 tabIndex={0}>I am a full-stack developer specializing in frontend technologies based in NYC.</h2>
-      <p tabIndex={0}>As a musician and former educator, I love discovering patterns to make code more efficient and creating delightful and intuitive UIs.</p>
-      <b tabIndex={0}>Click on a project below to learn more.</b>
+      <h1>Hi, I'm Adam! 👋🏼</h1>
+      <h2>I am a full-stack developer specializing in frontend technologies based in NYC.</h2>
+      <p>As a musician and former educator, I love discovering patterns to make code more efficient and creating delightful and intuitive UIs.</p>
+      <b>Click on a project below to learn more.</b>
     </header>
   );
 };
