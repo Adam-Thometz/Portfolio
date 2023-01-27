ec34fc784dd336e482c77944232e41fa2a32add5
Adam Thometz <adamthometz@gmail.com>
2023-01-27 08:06:35 -0500

    added resume

--- src/navbar/navbarInfo.js
+++ src/navbar/navbarInfo.js
@@ -1,22 +1,32 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub, faSoundcloud } from '@fortawesome/free-brands-svg-icons';
+import {
+  faLinkedin,
+  faGithub,
+  faSoundcloud,
+} from "@fortawesome/free-brands-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
 
 const links = [
   {
-    id: 'github',
+    id: "resume",
+    url: "https://docs.google.com/document/d/18oRb8BghIT_kmwTY2EXB03Lq9laVucn2t3BIKQmun7Y/edit?usp=sharing",
+    icon: <FontAwesomeIcon icon={faFile} />,
+  },
+  {
+    id: "github",
     url: "https://github.com/Adam-Thometz",
-    icon: <FontAwesomeIcon icon={faGithub} />
+    icon: <FontAwesomeIcon icon={faGithub} />,
   },
   {
-    id: 'linkedin',
+    id: "linkedin",
     url: "https://www.linkedin.com/in/adam-thometz/",
-    icon: <FontAwesomeIcon icon={faLinkedin} />
+    icon: <FontAwesomeIcon icon={faLinkedin} />,
   },
   {
-    id: 'soundcloud',
+    id: "soundcloud",
     url: "https://soundcloud.com/this-is-psychic-hamster",
-    icon: <FontAwesomeIcon icon={faSoundcloud} />
-  }
+    icon: <FontAwesomeIcon icon={faSoundcloud} />,
+  },
 ];
 
 export default links;
