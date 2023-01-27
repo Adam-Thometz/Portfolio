ff6d04fb00db2eaaa7d33680b35ee2b7fcd186e0
Adam Thometz <adamthometz@gmail.com>
2023-01-27 08:07:16 -0500

    added capitalized title

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import './Navbar.css';
+import "./Navbar.css";
 
-import links from './navbarInfo';
+import links from "./navbarInfo";
 
 const Navbar = ({ style }) => {
   return (
-    <nav className='Navbar' style={style}>
-      {links.map(link => (
-        <a
-          href={link.url}
-          aria-label={`Link to ${link.id}`}
-          className={`Navbar-link ${link.id}`}
-          title={link.id}
-          target="_blank"
-          rel='noreferrer'
-        >{link.icon}</a>
-      ))}
+    <nav className="Navbar" style={style}>
+      {links.map((link) => {
+        const name = link.id[0].toUpperCase() + link.id.slice(1);
+        return (
+          <a
+            href={link.url}
+            aria-label={`Link to ${name}`}
+            className={`Navbar-link ${link.id}`}
+            title={name}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {link.icon}
+          </a>
+        );
+      })}
     </nav>
   );
 };
