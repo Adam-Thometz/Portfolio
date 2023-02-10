14603f6e61a18c89d119450f00fb6363d7d5c882
Adam Thometz <adamthometz@gmail.com>
2023-02-09 20:17:54 -0500

    fixed bug

--- src/navbar/navbarInfo.js
+++ src/navbar/navbarInfo.js
@@ -25,25 +25,21 @@ const links = [
     id: "resume",
     url: resume,
     icon: <FontAwesomeIcon icon={faFile} />,
-    downloadable: true,
   },
   {
     id: "github",
     url: "https://github.com/Adam-Thometz",
     icon: <FontAwesomeIcon icon={faGithub} />,
-    downloadable: false,
   },
   {
     id: "linkedin",
     url: "https://www.linkedin.com/in/adam-thometz/",
     icon: <FontAwesomeIcon icon={faLinkedin} />,
-    downloadable: false,
   },
   {
     id: "soundcloud",
     url: "https://soundcloud.com/this-is-psychic-hamster",
     icon: <FontAwesomeIcon icon={faSoundcloud} />,
-    downloadable: false,
   },
 ];
 
