951c9ba98279c734d939b31dd1c7a33b46fb2292
Adam Thometz <adamthometz@gmail.com>
2023-02-09 19:00:50 -0500

    fixed bug

--- src/navbar/navbarInfo.js
+++ src/navbar/navbarInfo.js
@@ -6,26 +6,43 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
 
+// function download(url) {
+//   fetch(url).then((response) => {
+//     response.blob().then((blob) => {
+//       console.log(blob);
+//       const fileURL = window.URL.createObjectURL(blob);
+//       let aLink = document.createElement("a");
+//       aLink.href = fileURL;
+//       aLink.download = url;
+//       aLink.click();
+//     });
+//   });
+// }
+
 const links = [
   {
     id: "resume",
-    url: "resume-adam-thometz.pdf",
+    url: "https://drive.google.com/file/d/1EjQo4lWpYhJ2M2nsOfeJnh6ZebWKZ0gy/view",
     icon: <FontAwesomeIcon icon={faFile} />,
+    downloadable: true,
   },
   {
     id: "github",
     url: "https://github.com/Adam-Thometz",
     icon: <FontAwesomeIcon icon={faGithub} />,
+    downloadable: false,
   },
   {
     id: "linkedin",
     url: "https://www.linkedin.com/in/adam-thometz/",
     icon: <FontAwesomeIcon icon={faLinkedin} />,
+    downloadable: false,
   },
   {
     id: "soundcloud",
     url: "https://soundcloud.com/this-is-psychic-hamster",
     icon: <FontAwesomeIcon icon={faSoundcloud} />,
+    downloadable: false,
   },
 ];
 
