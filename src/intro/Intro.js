f0bda16529962b08ed224b6bb0ce79c18547d2db
Adam Thometz <adamthometz@gmail.com>
2023-02-22 09:59:57 -0500

    updated copy

--- src/intro/Intro.js
+++ src/intro/Intro.js
@@ -6,14 +6,7 @@ const Intro = ({ style }) => {
   return (
     <header className="Intro" style={style}>
       <h1>Hi, I'm Adam! 👋🏼</h1>
-      <h2>
-        I am a full-stack developer specializing in frontend technologies based
-        in NYC.
-      </h2>
-      <p>
-        As a musician and former educator, I love discovering patterns to make
-        code more efficient and creating delightful and intuitive UIs.
-      </p>
+      <h2>I am a frontend developer based in NYC.</h2>
       <b>Click on a project below to learn more.</b>
     </header>
   );
