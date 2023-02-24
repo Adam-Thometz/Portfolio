fbf21a615e8dfea90dd0a3a8366e6e38e09895d6
Adam Thometz <adamthometz@gmail.com>
2023-02-24 12:50:04 -0500

    update

--- src/intro/Intro.js
+++ src/intro/Intro.js
@@ -2,12 +2,18 @@ import React from "react";
 
 import "./Intro.css";
 
+import me from "../_media/imgs/adam.webp";
+
 const Intro = ({ style }) => {
   return (
     <header className="Intro" style={style}>
-      <h1>Hi, I'm Adam! 👋🏼</h1>
-      <h2>I am a frontend developer based in NYC.</h2>
-      <b>Click on a project below to learn more.</b>
+      <section className="Intro-top">
+        <aside>
+          <h1>Hi, I'm Adam! 👋🏼</h1>
+          <h2>I am a frontend developer based in NYC.</h2>
+        </aside>
+        <img src={me} alt="the developer" />
+      </section>
     </header>
   );
 };
