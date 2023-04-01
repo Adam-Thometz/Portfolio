f1b861b374fdc6b069d1bbe4f81fdc9b0f9fef91
Adam Thometz <adamthometz@gmail.com>
2023-04-01 15:13:49 -0400

    added new song

--- src/music/Music.js
+++ src/music/Music.js
@@ -8,6 +8,15 @@ const Music = ({ style }) => {
       <h2>Music</h2>
       <div className="Music-songs">
         <iframe
+        title="Plague of Lotuses"
+          width="100%"
+          height="200"
+          scrolling="no"
+          frameborder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1482866446&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          ></iframe>
+        <iframe
           title="Night Cycle"
           width="100%"
           height="200"
